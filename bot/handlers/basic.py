437f35b737fcf11ccd60d83e5d66103e6597690c
18kotov <twozerocool@gmail.com>
2023-06-09 10:38:24 +0300

    Добавил функцию быстрых повторов

--- bot/handlers/basic.py
+++ bot/handlers/basic.py
@@ -24,80 +24,88 @@ async def get_start(message: Message, bot):
     list_message = get_list_messages_for_today()
     await send_periodic_messages(bot, chat_id, list_message)
 
+@router.message(Command("all_days"), F.from_user.id.in_(users))
+async def get_start(message: Message, bot):
+    logger.debug('Получена комманда start')
+    await message.answer(f'Привет {message.from_user.first_name}. Начнем!')
+    chat_id = message.chat.id
+    list_message = get_list_messages_for_today()
+    await send_periodic_messages(bot, chat_id, list_message)
+
 
 @router.message(Command("1_day"), F.from_user.id.in_(users))
 async def day1(message: Message, bot):
-    logger.debug('Получена комманда 1_day')
+    logger.debug('Получена команда 1_day')
     await message.answer(f'Привет {message.from_user.first_name}. Начнем повтор вчерашних фраз!')
     chat_id = message.chat.id
     list_message = get_list_messages_day(1)
     await message.answer(f'Кол-во фраз {len(list_message)}')
-    await send_periodic_messages(bot, chat_id, list_message)
+    await send_messages_per_day(bot, chat_id, list_message)
 
 
 @router.message(Command("3_day"), F.from_user.id.in_(users))
 async def day3(message: Message, bot):
-    logger.debug('Получена комманда 3_day')
+    logger.debug('Получена команда 3_day')
     await message.answer(f'Привет {message.from_user.first_name}. Начнем повтор фраз добавленных 3 дня назад!')
     chat_id = message.chat.id
     list_message = get_list_messages_day(3)
     await message.answer(f'Кол-во фраз {len(list_message)}')
-    await send_periodic_messages(bot, chat_id, list_message)
+    await send_messages_per_day(bot, chat_id, list_message)
 
 
 @router.message(Command("7_day"), F.from_user.id.in_(users))
 async def day7(message: Message, bot):
-    logger.debug('Получена комманда 7_day')
+    logger.debug('Получена команда 7_day')
     await message.answer(f'Привет {message.from_user.first_name}. Начнем повтор фраз добавленных 7 дней назад!')
     chat_id = message.chat.id
     list_message = get_list_messages_day(7)
     await message.answer(f'Кол-во фраз {len(list_message)}')
-    await send_periodic_messages(bot, chat_id, list_message)
+    await send_messages_per_day(bot, chat_id, list_message)
 
 
 @router.message(Command("12_day"), F.from_user.id.in_(users))
 async def day12(message: Message, bot):
-    logger.debug('Получена комманда 12_day')
+    logger.debug('Получена команда 12_day')
     await message.answer(f'Привет {message.from_user.first_name}. Начнем повтор фраз добавленных 12 дней назад!')
     chat_id = message.chat.id
     list_message = get_list_messages_day(12)
     await message.answer(f'Кол-во фраз {len(list_message)}')
-    await send_periodic_messages(bot, chat_id, list_message)
+    await send_messages_per_day(bot, chat_id, list_message)
 
 
 @router.message(Command("16_day"), F.from_user.id.in_(users))
 async def day16(message: Message, bot):
-    logger.debug('Получена комманда 16_day')
+    logger.debug('Получена команда 16_day')
     await message.answer(f'Привет {message.from_user.first_name}. Начнем повтор фраз добавленных 16 дней назад!')
     chat_id = message.chat.id
     list_message = get_list_messages_day(16)
     await message.answer(f'Кол-во фраз {len(list_message)}')
-    await send_periodic_messages(bot, chat_id, list_message)
+    await send_messages_per_day(bot, chat_id, list_message)
 
 
 @router.message(Command("35_day"), F.from_user.id.in_(users))
 async def day35(message: Message, bot):
-    logger.debug('Получена комманда 35_day')
+    logger.debug('Получена команда 35_day')
     await message.answer(f'Привет {message.from_user.first_name}. Начнем повтор фраз добавленных 35 дней назад!')
     chat_id = message.chat.id
     list_message = get_list_messages_day(35)
     await message.answer(f'Кол-во фраз {len(list_message)}')
-    await send_periodic_messages(bot, chat_id, list_message)
+    await send_messages_per_day(bot, chat_id, list_message)
 
 
 @router.message(Command("50_day"), F.from_user.id.in_(users))
 async def day50(message: Message, bot):
-    logger.debug('Получена комманда 50_day')
+    logger.debug('Получена команда 50_day')
     await message.answer(f'Привет {message.from_user.first_name}. Начнем повтор фраз добавленных 50 дней назад!')
     chat_id = message.chat.id
     list_message = get_list_messages_day(50)
     await message.answer(f'Кол-во фраз {len(list_message)}')
-    await send_periodic_messages(bot, chat_id, list_message)
+    await send_messages_per_day(bot, chat_id, list_message)
 
 
 @router.message(Command("today"), F.from_user.id.in_(users))
 async def get_start(message: Message):
-    logger.debug('Получена комманда today')
+    logger.debug('Получена команда today')
     quantity = get_quantity_phrases_repeat_today()
     await message.answer(f'ок. сегодня повторяем фраз: {quantity}')
     logger.debug('Отправленно сообщение с количеством фраз')
@@ -112,6 +120,21 @@ async def send_periodic_messages(bot, chat_id, list_message):
         answer = await bot.send_message(chat_id, message['answer'])
         logger.debug(f'send answer')
         time.sleep(300)
+        await bot.delete_message(chat_id=chat_id, message_id=ask.message_id)
+        logger.debug(f'delete ask')
+        await bot.delete_message(chat_id=chat_id, message_id=answer.message_id)
+        logger.debug(f'delete answer')
+
+
+async def send_messages_per_day(bot, chat_id, list_message):
+    logger.debug(f'Длина выбранного списка фраз: {len(list_message)}')
+    for message in list_message:
+        ask = await bot.send_message(chat_id, message['ask'])
+        logger.debug(f'send ask')
+        time.sleep(20)
+        answer = await bot.send_message(chat_id, message['answer'])
+        logger.debug(f'send answer')
+        time.sleep(30)
         await bot.delete_message(chat_id=chat_id, message_id=ask.message_id)
         logger.debug(f'delete ask')
         await bot.delete_message(chat_id=chat_id, message_id=answer.message_id)
